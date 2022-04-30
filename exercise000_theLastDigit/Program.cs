b02287d2ecabf0a574499f95a1fb92ac10bb6149
maxim_stoma <maksim.stoma@mail.ru>
2022-04-30 12:09:59 +0300

    create exercise000_theLastDigit

--- exercise000_theLastDigit/Program.cs
+++ exercise000_theLastDigit/Program.cs
@@ -1,1 +1,6 @@
 ﻿ Console.WriteLine("Enter the number ");
+ int num = Convert.ToInt32(Console.ReadLine());
+ int digit = num % 10;
+
+ Console.Write("The last digit of number is ");
+ Console.WriteLine(digit);
