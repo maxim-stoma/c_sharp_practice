0aeb92f583ebd05d1e6e996440fe872923dedb2a
maxim_stoma <maksim.stoma@mail.ru>
2022-05-01 19:27:50 +0300

    edit comments in task000, create task002_multipies

--- exercise000_theLastDigit/Program.cs
+++ exercise000_theLastDigit/Program.cs
@@ -1,6 +1,6 @@
-﻿ Console.WriteLine("Enter the number ");
- int num = Convert.ToInt32(Console.ReadLine());
- int digit = num % 10;
+﻿Console.WriteLine("Enter the number ");
+int num = Convert.ToInt32(Console.ReadLine());
+int digit = num % 10;
 
- Console.Write("The last digit of number is ");
- Console.WriteLine(digit);
+Console.Write("The last digit of number is ");
+Console.WriteLine(digit);
---
+++ exercise002_multiplies/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int mult = num1 % num2;
+if(mult == 0) Console.WriteLine(num2 + " times " + num1);
+else Console.WriteLine(num2 + " is not a multiple of " + num1 + ", the remainder is " + mult);
+
+Console.ReadLine();
