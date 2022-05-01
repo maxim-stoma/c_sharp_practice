8ef68a03590a7ea439fe8c675cfb1458e8eb9ed3
maxim_stoma <maksim.stoma@mail.ru>
2022-05-01 21:22:42 +0300

    create exercise003_multiple_7_23

---
+++ exercise003_multiple_7_23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int mult7 = num % 7;
+int mult23 = num % 23;
+
+if (mult7 == mult23) Console.WriteLine(num + " кратно 7 и 23 одновременно");
+else Console.WriteLine(num + " не кратно 7 и 23 одновременно");
