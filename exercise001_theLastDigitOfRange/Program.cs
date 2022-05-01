a1b8d7806d1728b75ff232a7c7e9180bdb56810c
maxim_stoma <maksim.stoma@mail.ru>
2022-05-01 18:45:30 +0300

    modify task001: edit comments

--- exercise001_theLastDigitOfRange/Program.cs
+++ exercise001_theLastDigitOfRange/Program.cs
@@ -1,14 +1,15 @@
-﻿int num = new Random().Next(10, 99);
+﻿Console.WriteLine("Range of numbers from 10 to 99.");
+int num = new Random().Next(10, 99);
 int digit1 = num / 10;
 int digit2 = num % 10;
 
-Console.WriteLine("Chosen number is " + num);
-Console.WriteLine("The first digit is " + digit1);
-Console.WriteLine("The second digit is " + digit2);
+Console.WriteLine("Chosen number is " + num + ".");
+Console.WriteLine("The first digit is " + digit1 + ";");
+Console.WriteLine("The second digit is " + digit2 + ";");
 
 if (digit1 == digit2)
-    Console.WriteLine("Digits are equal");
+    Console.WriteLine("Digits are equal.");
 else if (digit1 > digit2)
-    Console.WriteLine(digit1 + " is greater");
+    Console.WriteLine(digit1 + " is greater.");
 else
-    Console.WriteLine(digit2 + " is greater");
+    Console.WriteLine(digit2 + " is greater.");
