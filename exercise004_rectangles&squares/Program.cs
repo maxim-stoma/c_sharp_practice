3251b785c9b5fbea3c47085f4eefa2b62e5b8edc
maxim_stoma <maksim.stoma@mail.ru>
2022-05-02 12:41:21 +0300

    create exercise004_rectangles&squares

---
+++ exercise004_rectangles&squares/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите большую сторону прямоугольника: ");
+int rectA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите меньшую сторону прямоугольника: ");
+int rectB = Convert.ToInt32(Console.ReadLine());
+
+if(rectB > rectA) Console.WriteLine("И как у тебя меньшая сторона больше большей, умник?");
+else
+{
+    Console.Write("Введите сторону квадрата: ");
+    int sqar = Convert.ToInt32(Console.ReadLine());
+    int countA = rectA / sqar;
+    int countB = rectB / sqar;
+
+    if (sqar > rectA) Console.WriteLine("Не получится вырезать квадрат из прямоугольника такого размера.");
+    else if (sqar > rectB) Console.WriteLine("Не получится вырезать квадрат из прямоугольника такого размера.");
+    else
+    {
+        int count = countA * countB;
+        Console.WriteLine($"{count} квадратов можно вырезать из прямоугольника");
+    }
+}
+Console.ReadLine();
