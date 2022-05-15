84995119aecbd24061c8e4047020a21230e06fbf
maxim_stoma <maksim.stoma@mail.ru>
2022-05-15 22:19:46 +0300

    add exercise 007 solution

---
+++ exercise007_logic2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+bool A = true;
+bool B = false;
+bool C = false;
+
+Console.WriteLine($"Первое логическое выражение не А и В равно {!A & B}");
+Console.WriteLine($"Второе логическое выражение А или не В равно {A & !B}");
+Console.WriteLine($"Второе логическое выражение А или не В равно {A & B | C}");
+
+Console.ReadLine();
