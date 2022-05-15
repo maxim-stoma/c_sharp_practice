3539362e521d6f96b53806cdfefb56d1f740cb12
maxim_stoma <maksim.stoma@mail.ru>
2022-05-15 21:12:41 +0300

    add an exercise 006 solution

---
+++ exercise006_logic1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+bool A = true;
+bool B = false;
+bool C = false;
+
+Console.WriteLine($"Первое логическое выражение А или В равно {A ^ B}");
+Console.WriteLine($"Второе логическое выражение А и В равно {A & B}");
+Console.WriteLine($"Третье логическое выражение В или С равно {B ^ C}");
+
+Console.ReadLine();
