f04a85e97739a8ecf1880c8a7c8a10ce5ee60456
maxim_stoma <maksim.stoma@mail.ru>
2022-05-15 14:34:04 +0300

    add new solution of the secondexercise

---
+++ exercise001.1_newWay/Program.cs
@@ -0,0 +1,13 @@
+﻿int randomNum = new Random().Next(10, 100);
+
+int num1 = randomNum % 10;
+int num2 = randomNum / 10;
+Console.Write($"У числа {randomNum} большая цифра ");
+if (num1 > num2)
+{
+    Console.Write(num1);
+}
+else
+{
+    Console.Write(num2);
+}
