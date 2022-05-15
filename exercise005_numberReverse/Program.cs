3b32faace5a2145795e780b30ea525391b51d464
maxim_stoma <maksim.stoma@mail.ru>
2022-05-15 15:13:30 +0300

    create exercise005_numberReverse

---
+++ exercise005_numberReverse/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.Write("Это похоже на трёхзначное число?");
+}
+else if (num > 999)
+{
+    Console.Write("Тут явно не три цифры.");
+}
+else
+{
+    int firstDigit = num / 100;
+    int secondDigit = (num / 10) % 10;
+    int thirdDigit = num % 10;
+    int reverse = (thirdDigit * 100) + (secondDigit * 10) + firstDigit;
+    Console.WriteLine($"Обратное число {reverse}");
+}
+Console.ReadLine();
